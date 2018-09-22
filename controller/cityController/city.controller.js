2691826ca6abbb4ca429d33c0b87b58c4227133f
Amit <jsonamit@gmail.com>
2018-09-22 11:23:39 +0530

    added some route

--- controller/cityController/city.controller.js
+++ controller/cityController/city.controller.js
@@ -1,3 +1,6 @@
 module.exports.getcity = (req,res) => {
     res.send('this is city controller');
 }
+module.exports.getcityById = (req,res) => {
+    res.send('getting by city from city controller');
+}
--- routes/cityRoutes/city.route.js
+++ routes/cityRoutes/city.route.js
@@ -4,5 +4,6 @@ const route = express.Router();
 const controller = require('../../controller/cityController/city.controller');
 
 route.get('/', controller.getcity);
+route.get('/cityById', controller.getcityById);
 
 module.exports = route;
