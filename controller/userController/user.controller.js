1bc11f69414d23e39ba343be77dc56969ca72e12
Amit <jsonamit@gmail.com>
2018-09-25 11:35:15 +0530

    set response

--- controller/userController/user.controller.js
+++ controller/userController/user.controller.js
@@ -3,7 +3,29 @@
 
 
 module.exports.getuser = (req,res) => {
-    res.send('this is user route');
+    var response={};
+    try    {
+
+        if(req){
+
+            response.message= "getting user successfully";
+            response.status= 200;
+            res.send(response);
+        }
+        else
+        {
+            response.message= "some data missing";
+            response.status= 404;
+            res.send(response);
+        }
+    }
+    catch(error)
+    {
+        response.message= "server error";
+        response.status= 500;
+        response.data= error;
+        res.send(response);
+    }
     // user.find((err,doc)=>{
     //     if(!err)
     //     {
